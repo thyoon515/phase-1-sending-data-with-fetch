3e52bf89729e1157c3ec98b4158f0128f4a8edeb
thyoon515 <thyoon515@gmail.com>
2022-05-05 11:36:51 -0400

    commit tree 48acaeeb5fd53512c63b7720572284740122a794

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(name, email) {
+
+const conObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        "name": name,
+        "email": email,
+    }),
+};
+
+return fetch("http://localhost:3000/users", conObj)
+    .then(res => res.json())
+    .then(body => handleNewUser(body.id))
+    .catch(error => handleError(error))
+}
+
+function handleNewUser(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+    document.body.append();
+}
+
+function handleError(){
+    let message = 'Unauthorized Access'
+    document.getElementsByTagName("body")[0].innerHTML = message;
+    document.body.append();
+    return message;
+}
+
+
+submitData("steve", "steven@steven.com")
